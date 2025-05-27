c2393fe5e50a23d64e8768022342fce6d13bd75f
Felipe Andrade <fe.a.andrade24@gmail.com>
2025-05-27 18:43:49 -0300

    chore: aplicando melhorias sugeridas na ultima revisao

--- app/models/frete_model.py
+++ app/models/frete_model.py
@@ -4,4 +4,4 @@ from . import PersistableEntity
 class Frete(PersistableEntity):
     seller_id: str
     sku: str
-    valor_frete: int
+    valor: int
--- app/services/frete/frete_exceptions.py
+++ app/services/frete/frete_exceptions.py
@@ -1,5 +1,5 @@
 from app.api.common.schemas.response import ErrorDetail
-from app.common.exceptions import ConflictException
+from app.common.exceptions import ConflictException, NotFoundException
 
 class FreteAlreadyExistsException(ConflictException):
     def __init__(
@@ -15,6 +15,23 @@ class FreteAlreadyExistsException(ConflictException):
                 location=location,
                 slug=slug,
                 field=field,
+            )
+        ]
+        super().__init__(details=details)
+
+class FreteNotFoundException(NotFoundException):
+    def __init__(
+        self,
+        seller_id: str,
+        sku: str,
+    ):
+        details = [
+            ErrorDetail(
+                message="Frete para produto não encontrado.",
+                location="path",
+                slug="frete_nao_encontrado",
+                field="sku",
+                ctx={"seller_id": seller_id, "sku": sku},
             )
         ]
         super().__init__(details=details)
